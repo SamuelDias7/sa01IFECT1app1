8a77deb0569e9a1984af90426ae0d595188426d6
Samuel <samuelhenrique1234765@gmail.com>
2023-04-14 17:27:44 -0300

    Atualizando codigo

--- src/br/com/IFECT1/bo/Calculo.java
+++ src/br/com/IFECT1/bo/Calculo.java
@@ -12,35 +12,73 @@ public class Calculo {
 	public int resultotal;
 	
 	public void calculo() {
-		totalmes = 30 * 2;
+		totalmes = 30 * 0;
 		
-		if(this.mes == 3) {
+		if(this.mes == 1) {
 			this.total=+ totalmes;
 			this.total = this.dia + this.total;
-			System.out.println("Total1: "  + this.total);
 		}
 		
+		else if(this.mes == 2) {
+			totalmes = 30 * 1;
+			
+			this.total=+ totalmes;
+			this.total = this.dia + this.total;
+		}
+		else if(this.mes == 3) {
+			totalmes = 30 * 2;
+			
+			this.total=+ totalmes;
+			this.total = this.dia + this.total;
+		}
 		else if(this.mes == 4) {
 			totalmes = 30 * 3;
 			
 			this.total=+ totalmes;
 			this.total = this.dia + this.total;
-			System.out.println("Total1: "  + this.total);
 		}
-			
 		else if(this.mes == 5) {
 			totalmes = 30 * 4;
+			
 			this.total=+ totalmes;
 			this.total = this.dia + this.total;
-			System.out.println("Total1: "  + this.total);
+		}
+		else if(this.mes == 6) {
+			totalmes = 30 * 5;
+			
+			this.total=+ totalmes;
+			this.total = this.dia + this.total;
+		}
+		else if(this.mes == 7) {
+			totalmes = 30 * 6;
+			
+			this.total=+ totalmes;
+			this.total = this.dia + this.total;
+		}
+		else if(this.mes == 8) {
+			totalmes = 30 * 7;
+			
+			this.total=+ totalmes;
+			this.total = this.dia + this.total;
+		}
+		else if(this.mes == 9) {
+			totalmes = 30 * 8;
+			
+			this.total=+ totalmes;
+			this.total = this.dia + this.total;
+		}
+			
+		else if(this.mes == 10) {
+			totalmes = 30 * 9;
+			this.total=+ totalmes;
+			this.total = this.dia + this.total;
 			
 		}else{
 			System.out.println("Erro");
 			
 		}
 		
-		totalano = totalano - ano; 
-		totalano *= 365;
+		totalano = ano * 365; 
 		
 		resultotal = totalmes + totalano;
 		
